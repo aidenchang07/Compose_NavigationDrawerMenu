88b2cf0d8389edc2bef25c93c4d0c950d298d5e4
Aiden Chang <asdf62210@gmail.com>
2023-12-17 11:24:10 +0800

    Add MainScreen

---
+++ app/src/main/java/com/example/compose_navigationdrawermenu/ui/screens/MainScreen.kt
@@ -0,0 +1,24 @@
+package com.example.compose_navigationdrawermenu.ui.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Created by AidenChang 2023/12/17
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold { it
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
